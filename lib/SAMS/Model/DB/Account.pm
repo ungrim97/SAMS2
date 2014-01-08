23e59190767e83b3ba0cbe352022a09b7b964117
Mike Francis <ungrim97@gmail.com>
2014-01-08 13:45:05 +0000

    SAM-2361: Moved update account logic to DB::Account class

--- lib/SAMS/Model/DB/Account.pm
+++ lib/SAMS/Model/DB/Account.pm
@@ -1,8 +1,52 @@
 package SAMS::Model::DB::Account;
+use SAMS::Error;
+
+=head1 NAME
+
+SAMS::Model::DB::Account
+
+=head1 DESCRIPTION
+
+Custom methods for implementing on a single DBIC result object.
+
+=cut
 
 sub is_authorised {
 
     return 1
 }
 
+sub update_account {
+    my ($self, %input) = @_;
+    my $params = $input{params};
+    my $user   = $input{user};
+
+    # Replace with action in params
+    my $action = $self->can('update_contact_details');
+
+    return SAMS::Error->new(
+        level           => 'Error',
+        error_message   => 'No Account to update',
+    ) unless $self->account_id;
+
+    return SAMS::Error->new(
+        level           => 'Error',
+        error_message   => 'User not authorised to edit account '.$account->account_id,
+    ) unless $user->is_authorised(update => $account);
+
+    $self->$action($params, $user);
+}
+
+sub update_contact_details {
+    my ($self, $params, $user) = @_;
+
+    # Remove any params that aren't account columns
+    for my $param (keys $params){
+        delete $params->{$param} unless $self->has_column($param);
+    }
+
+    $self->update($params);
+
+    return $self;
+}
 1;
--- lib/SAMS/Schema/ResultSet/Account.pm
+++ lib/SAMS/Schema/ResultSet/Account.pm
@@ -13,37 +13,3 @@ sub BUILDARGS {
     $_[2];
 }
 
-sub update_account {
-    my ($self, %input) = @_;
-    my $params = $input{params};
-    my $user   = $input{user};
-
-    my $action = $self->can('update_contact_details');
-
-    return SAMS::Error->new(
-        level           => 'Error',
-        error_message   => 'No Account to update',
-    ) unless $params->{account_id};
-
-    my $account = $self->find($params->{account_id});
-
-    return SAMS::Error->new(
-        level           => 'Error',
-        error_message   => 'User not authorised to edit account '.$params->{account_id},
-    ) unless $user->is_authorised(update => $account);
-
-    $self->$action($account, $params, $user);
-}
-
-sub update_contact_details {
-    my ($self, $account, $params, $user) = @_;
-
-    # Remove any params that aren't account columns
-    for my $param (keys $params){
-        delete $params->{$param} unless $account->has_column($param);
-    }
-
-    $account->update($params);
-
-    return $account;
-}
