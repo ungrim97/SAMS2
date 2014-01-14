c0c45ce0503eb49b6b1b798f4e35e88b8dfddef8
Mike Francis <ungrim97@gmail.com>
2014-01-14 21:41:42 +0000

    Remove test sql dump from DeploymentHandler in preperation for use of DBIx::Class::Migration

--- sql/_common/deploy/1/004_create_account_types.pl
+++
@@ -1,8 +0,0 @@
-sub {
-    my ($schema, $version) = @_;
-
-    $schema->resultset('AccountType')->populate([
-            ['account_type_id', 'description', 'last_update_time'],
-            ['1', 'Institution', '2014-01-07 15:11:14.648109+00'],
-        ]);
-}
